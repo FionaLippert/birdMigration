9fd23a7ca11fc1e6304e9bc0b7d10b679045f31c
FionaLippert <fiona@lipperta.de>
2020-04-06 18:13:15 +0200

    select quantity for vertical integration

--- preprocessing/generate_radar_images.R
+++ preprocessing/generate_radar_images.R
@@ -39,10 +39,10 @@ vertical_integration <- function(timestamp){
         result <- integrate_to_ppi(raster = grid,
                          pvol = retrieve_pvol(vp_key_to_pvol(k)),
                          vp = retrieve_vp(k))
-        strs <- st_as_stars(result$data[[config$quantity]])
+        strs <- st_as_stars(result$data)
         #message(length(result$data[[config$quantity]]))
-        #fname <- paste0(config$quantity, "_", timestamp, ".tif")
-        #write_stars(strs, file.path(path, fname), driver = "GTiff")
+        fname <- paste0(config$quantity, "_", timestamp, ".tif")
+        write_stars(strs[config$quantity], file.path(path, fname), driver = "GTiff")
       }
     }
 }
