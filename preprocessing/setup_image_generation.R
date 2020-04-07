5b493379c8c46eb5076d3ea5ef723ddbc28059c5
FionaLippert <fiona@lipperta.de>
2020-04-07 14:36:13 +0200

    save as hdf5

--- preprocessing/setup_image_generation.R
+++ preprocessing/setup_image_generation.R
@@ -6,7 +6,7 @@ require(uvaRadar)
 require(stars)
 require(yaml)
 
-config_file <- file(file.path(args[1], "config.yml"), open="rw")
+config_file <- file.path(args[1], "config.yml")
 config = yaml.load_file(config_file) #"config.yml")
 
 # set credentials for UvA Radar Data Storage
@@ -17,4 +17,3 @@ bbox <- st_bbox(get_radars_df(config$radars)$geometry)
 config[['bounds']] <- bbox
 
 write_yaml(config, config_file)
-close(config_file)
