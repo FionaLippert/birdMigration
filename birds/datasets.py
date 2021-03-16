fixed normalization of static features in new dataloader

birds/datasets.py
@@ -187,7 +187,7 @@ class RadarData(InMemoryDataset):
         if self.use_buffers:
             self.processed_dirname = f'measurements=from_buffers_split={split}'
         else:
-            self.processed_dirname = 'measurements=voronoi_cells'
+            self.processed_dirname = f'measurements=voronoi_cells'
 
         super(RadarData, self).__init__(root, transform, pre_transform)
 
@@ -250,6 +250,7 @@ class RadarData(InMemoryDataset):
         # normalize dynamic features
         cidx = ~dynamic_feature_df.columns.isin(['birds', 'birds_from_buffer', 'radar', 'night',
                                                  'dusk', 'dawn', 'datetime'])
+
         dynamic_feature_df.loc[:, cidx] = dynamic_feature_df.loc[:, cidx].apply(
             lambda col: (col - col.min()) / (col.max() - col.min()), axis=0)
         dynamic_feature_df['birds'] = dynamic_feature_df.birds / self.bird_scale
@@ -264,8 +265,12 @@ class RadarData(InMemoryDataset):
 
         time = dynamic_feature_df.datetime.sort_values().unique()
         tidx = np.arange(len(time))
-        areas = voronoi.area_km2.to_numpy()
-        coords = voronoi[coord_cols].to_numpy()
+
+        # normalize static features
+        cidx = ['area_km2', *coord_cols]
+        static = voronoi.loc[:, cidx].apply(lambda col: (col - col.min()) / (col.max() - col.min()), axis=0)
+        areas = static.area_km2.to_numpy()
+        coords = static[coord_cols].to_numpy()
 
         inputs = []
         targets = []

scripts/graphNN_experiments.py
@@ -61,7 +61,7 @@ def persistence(last_ob, timesteps):
 def run_training(timesteps, model_type, conservation=True, recurrent=True, embedding=0, norm=False, epochs=100,
                  repeats=1, data_source='radar', output_dir=model_dir, bird_scale=2000, departure=False):
 
-    train_data = [RadarData(root, 'train', year, season, timesteps, data_source=data_source,
+    train_data = [datasets.RadarData(root, 'train', year, season, timesteps, data_source=data_source,
                             use_buffers=args.use_buffers, bird_scale=bird_scale) for year in train_years]
     boundaries = train_data[0].info['boundaries']
     if args.fix_boundary:
@@ -71,7 +71,7 @@ def run_training(timesteps, model_type, conservation=True, recurrent=True, embed
     train_data = torch.utils.data.ConcatDataset(train_data)
     train_loader = DataLoader(train_data, batch_size=1, shuffle=True)
 
-    val_data = RadarData(root, 'test', val_year, season, timesteps, data_source=data_source,
+    val_data = datasets.RadarData(root, 'test', val_year, season, timesteps, data_source=data_source,
                                   bird_scale=bird_scale, use_buffers=args.use_buffers)
     val_loader = DataLoader(val_data, batch_size=1)
 