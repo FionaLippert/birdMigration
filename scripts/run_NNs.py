f66112ede85106641faafeaf6e626d978a3ff86d
FionaLippert <fiona@lipperta.de>
2021-04-13 09:08:01 +0200

    load trained models directly from grid search

--- scripts/run_NNs.py
+++ scripts/run_NNs.py
@@ -165,7 +165,9 @@ def train(cfg: DictConfig, output_dir: str, log):
         yaml.dump(model_config, f)
 
     # save complete config to output dir
-    with open(osp.join(output_dir, f'config.yaml'), 'w') as f:
+    for key, val in best_hp_settings.items():
+        cfg.model.hyperparameters[key]['default'] = val
+    with open(osp.join(output_dir, f'best_config.yaml'), 'w') as f:
         OmegaConf.save(config=cfg, f=f)
 
     log.flush()
