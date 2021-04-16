fix root normalization

scripts/run_GBT.py
@@ -62,7 +62,7 @@ def train(cfg: DictConfig, output_dir: str, log):
     if cfg.root_transform == 0:
         cfg.datasource.bird_scale = float(normalization.max('birds'))
     else:
-        cfg.datasource.bird_scale = float(normalization.root_max('birds'))
+        cfg.datasource.bird_scale = float(normalization.root_max('birds', cfg.root_transform))
 
 
     best_val_loss = np.inf
@@ -150,7 +150,7 @@ def test(cfg: DictConfig, output_dir: str, log):
     if cfg.root_transform == 0:
         cfg.datasource.bird_scale = float(normalization.max('birds'))
     else:
-        cfg.datasource.bird_scale = float(normalization.root_max('birds'))
+        cfg.datasource.bird_scale = float(normalization.root_max('birds', cfg.root_transform))
 
     # load test data
     test_data = datasets.RadarData(data_root, str(cfg.datasource.test_year),