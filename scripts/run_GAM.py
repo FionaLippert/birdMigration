6d074a094b8a6b4b6dc3fe7da2d11abf9097510c
FionaLippert <fiona@lipperta.de>
2021-04-29 15:50:00 +0200

    debugging

--- scripts/run_GAM.py
+++ scripts/run_GAM.py
@@ -54,6 +54,7 @@ def train(cfg: DictConfig, output_dir: str, log):
     all_mappings = []
     for idx, data in enumerate(train_data_list):
         X_train, y_train, mask_train = GBT.prepare_data_gam(data, timesteps=ts, mask_daytime=False)
+        print(X_train.shape)
         all_X.append(X_train)
         all_y.append(y_train)
         all_masks.append(mask_train)
