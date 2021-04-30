e1e56badf5a3d991187bd72e34eafdf8977d0939
FionaLippert <fiona@lipperta.de>
2021-04-30 13:37:26 +0200

    debugging

--- birds/graphNN.py
+++ birds/graphNN.py
@@ -854,6 +854,7 @@ class BirdFluxGraphLSTM(MessagePassing):
         flux = self.fc_edge_out(flux).tanh()
 
         # enforce fluxes to be symmetric along edges
+        print(flux.shape)
         A_flux = to_dense_adj(self.edges, edge_attr=flux.view(-1))
         A_flux = torch.triu(A_flux, diagonal=1) # values on diagonal are zero
         A_flux = A_flux - A_flux.T
