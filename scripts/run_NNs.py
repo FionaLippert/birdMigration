76bfd9d6839835fd0bccf13682a5f941f7de8b78
FionaLippert <fiona@lipperta.de>
2021-05-03 17:20:50 +0200

    debugging

--- scripts/run_NNs.py
+++ scripts/run_NNs.py
@@ -129,7 +129,8 @@ def train(cfg: DictConfig, output_dir: str, log):
 
             print(f'train model [trial {r}]')
             print(cfg.datasource.env_vars)
-            model = Model(**hp_settings, timesteps=cfg.model.horizon, seed=(cfg.seed + r), n_env=2+len(cfg.datasource.env_vars),
+            model = Model(**hp_settings, timesteps=cfg.model.horizon, seed=(cfg.seed + r),
+                          n_env=2+len(cfg.datasource.env_vars)+len(additional_env_vars),
                           fixed_boundary=boundary if fixed_boundary else [], force_zeros=cfg.model.get('force_zeros', 0),
                           edge_type=cfg.edge_type, use_encoder=use_encoder, t_context=context)
 
