bb2b906cbd23dd3b0c4c703190d20052cf0a0572
FionaLippert <fiona@lipperta.de>
2021-05-03 18:07:23 +0200

    accumulation variables

--- scripts/run_NNs.py
+++ scripts/run_NNs.py
@@ -128,7 +128,7 @@ def train(cfg: DictConfig, output_dir: str, log):
             print(f'train model [trial {r}]')
             print(cfg.datasource.env_vars)
             model = Model(**hp_settings, timesteps=cfg.model.horizon, seed=(cfg.seed + r),
-                          n_env=2+len(cfg.datasource.env_vars)+len(additional_env_vars),
+                          n_env=2+len(cfg.datasource.env_vars),
                           fixed_boundary=boundary if fixed_boundary else [], force_zeros=cfg.model.get('force_zeros', 0),
                           edge_type=cfg.edge_type, use_encoder=use_encoder, t_context=context,
                           use_acc_vars=cfg.model.get('use_acc_vars', False))
