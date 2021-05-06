398219a31816dbbcfdbeef47c61483ce20412a68
FionaLippert <fiona@lipperta.de>
2021-05-06 16:13:59 +0200

    debugging

--- birds/graphNN.py
+++ birds/graphNN.py
@@ -864,15 +864,15 @@ class BirdFluxGraphLSTM(MessagePassing):
 
         flux = self.fc_edge_out(flux) #.tanh()
 
-        if self.enforce_conservation:
-            # enforce fluxes to be symmetric along edges
-            A_flux = to_dense_adj(self.edges, edge_attr=flux).squeeze()
-            A_flux = torch.triu(A_flux, diagonal=1) # values on diagonal are zero
-            A_flux = A_flux - A_flux.T
-            #edge_index, flux = dense_to_sparse(A_flux)
-
-            flux = A_flux[self.edges[0], self.edges[1]]
-            flux = flux.view(-1, 1)
+        # if self.enforce_conservation:
+        #     # enforce fluxes to be symmetric along edges
+        #     A_flux = to_dense_adj(self.edges, edge_attr=flux).squeeze()
+        #     A_flux = torch.triu(A_flux, diagonal=1) # values on diagonal are zero
+        #     A_flux = A_flux - A_flux.T
+        #     #edge_index, flux = dense_to_sparse(A_flux)
+        #
+        #     flux = A_flux[self.edges[0], self.edges[1]]
+        #     flux = flux.view(-1, 1)
 
         self.local_fluxes[..., t] = flux
 
