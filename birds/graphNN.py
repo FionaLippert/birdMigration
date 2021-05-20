3bedaa3839d15f74b5f377bcea99a12b4aff27d3
FionaLippert <fiona@lipperta.de>
2021-05-20 13:23:22 +0200

    add perturbations to boundary

--- birds/graphNN.py
+++ birds/graphNN.py
@@ -946,6 +946,7 @@ class BirdFluxGraphLSTM(MessagePassing):
             if len(self.fixed_boundary) > 0:
                 # use ground truth for boundary nodes
                 perturbation = torch.randn(len(self.fixed_boundary)) * self.perturbation_std + self.perturbation_mean
+                print(perturbation)
                 x[self.fixed_boundary, 0] = data.y[self.fixed_boundary, t] + perturbation
 
             if self.force_zeros:
