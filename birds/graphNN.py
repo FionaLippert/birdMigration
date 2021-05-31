87c23a12097d61d48f92cd64018f13f648bd9a96
FionaLippert <fiona@lipperta.de>
2021-05-31 15:31:04 +0200

    debugging

--- birds/graphNN.py
+++ birds/graphNN.py
@@ -2783,7 +2783,6 @@ def train_fluxes(model, train_loader, optimizer, loss_func, device, conservation
             mask = mask[:, model.t_context:]
         #print(diff.size(), loss_func(output, gt, mask).detach(), constraints.detach())
         constraints = conservation_constraint * constraints
-        print(constraints.detach())
         loss = loss_func(output, gt, mask) + constraints
 
         loss.backward()
