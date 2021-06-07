5ff61330f8a698e22551957eac5975945794bf68
FionaLippert <fiona@lipperta.de>
2021-06-07 10:27:16 +0200

    adjusted slurm module compatibility

--- scripts/test_script.py
+++ scripts/test_script.py
@@ -2,24 +2,24 @@ import os.path as osp
 import os
 import traceback
 
-import logging
+#import logging
 import time
 
 import hydra
 import submitit
 from omegaconf import DictConfig, OmegaConf
 
-log = logging.getLogger(__name__)
+#log = logging.getLogger(__name__)
 
 
-@hydra.main(config_path=".", config_name="config")
+@hydra.main(config_path="conf", config_name="config")
 def run(cfg: DictConfig):
-    env = submitit.JobEnvironment()
-    log.info(f"Process ID {os.getpid()} executing task {cfg.task}, with {env}")
-
-    # output_dir = osp.join(cfg.root, 'results', cfg.datasource.name, cfg.action.name,
-    #                       cfg.model.name, cfg.experiment)
-    # os.makedirs(output_dir, exist_ok=True)
+    # env = submitit.JobEnvironment()
+    # log.info(f"Process ID {os.getpid()} executing task {cfg.task}, with {env}")
+    print(cfg)
+    output_dir = osp.join(cfg.root, 'results', cfg.datasource.name, cfg.action.name,
+                          cfg.model.name, cfg.experiment)
+    os.makedirs(output_dir, exist_ok=True)
 
     time.sleep(1)
 
