5b21d7ef176a34dc15fb1f13bf9f34d1139e693c
FionaLippert <fiona@lipperta.de>
2021-06-15 13:01:05 +0200

    debugging

--- birds/graphNN.py
+++ birds/graphNN.py
@@ -1329,6 +1329,7 @@ class BirdFluxGraphLSTM(MessagePassing):
         # enforce fluxes to be symmetric along edges
         flux = flux.sigmoid() # bird density flying from node j to node i should be positive
         flux = flux * x_j
+        print(f'flux = {flux}')
         # self.local_fluxes_A[self.edges[0], self.edges[1]] = flux.squeeze()
 
         if self.boundary_model == 'FluxMLP':
@@ -1416,6 +1417,7 @@ class BirdFluxGraphLSTM(MessagePassing):
         if not self.training:
             self.local_deltas[..., t] = delta
 
+        print(f'delta = {delta}')
         pred = x + delta + aggr_out # take messages into account for inner cells only
         #pred = pred.relu() # enforce positive bird densities
 
