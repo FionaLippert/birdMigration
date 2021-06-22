1752e3599766e07586c3b717cd4972e68e1393eb
FionaLippert <fiona@lipperta.de>
2021-06-22 20:23:52 +0200

    separate output dirs for parallel jobs

--- scripts/run_2.py
+++ scripts/run_2.py
@@ -8,15 +8,16 @@ import run_NNs_2
 @hydra.main(config_path="conf2", config_name="config")
 def run(cfg: DictConfig):
 
-    print(f'output directory: {cfg.output_dir}')
-    os.makedirs(osp.join(cfg.output_dir, f'job_{cfg.get("job_id", 0)}'), exist_ok=True)
+    out = osp.join(cfg.output_dir, f'job_{cfg.get("job_id", 0)}')
+    print(f'output directory: {out}')
+    os.makedirs(out, exist_ok=True)
 
-    log_file = os.path.join(cfg.output_dir, 'log.txt')
+    log_file = os.path.join(out, 'log.txt')
     log = open(log_file, 'w')
 
     try:
-        run_NNs_2.train(cfg, cfg.output_dir, log)
-        run_NNs_2.test(cfg, cfg.output_dir, log)
+        run_NNs_2.train(cfg, out, log)
+        run_NNs_2.test(cfg, out, log)
     except Exception:
         print(traceback.format_exc(), file=log)
     print('flush log')
