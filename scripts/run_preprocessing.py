a124dbb4fefcf7ea9e79c5f227a1babda54c29ce
FionaLippert <fiona@lipperta.de>
2021-09-27 14:05:06 +0200

    load era5 in preprocessing script

--- scripts/run_preprocessing.py
+++ scripts/run_preprocessing.py
@@ -1,9 +1,9 @@
-from birds import datasets
+from birds import datasets, era5interface
 from omegaconf import DictConfig, OmegaConf
 import hydra
 import os.path as osp
 import os
-
+import geopandas as gpd
 
 
 @hydra.main(config_path="conf", config_name="config")
@@ -12,7 +12,17 @@ def run(cfg: DictConfig):
     years = cfg.datasource.years
     print('preprocess data for years', years)
     data_root = osp.join(cfg.device.root, 'data')
+
+    if cfg.get('download_era5', False):
+        df = gpd.read_file(osp.join(data_root, 'raw', 'abm', 'all_radars.shp'))
+        radars = dict(zip(zip(df.lon, df.lat), df.radar.values))
+        dl = era5interface.ERA5Loader(radars)
+        
     for year in years:
+        if cfg.get('download_era5', False):
+            env_dir = osp.join(data_root, 'raw', 'env', cfg.season, year)
+            dl.download_season(cfg.season, year, env_dir, buffer_x=4, buffer_y=4, surface_data=True)
+
         target_dir = osp.join(data_root, 'preprocessed',
                               f'{cfg.t_unit}_{cfg.model.edge_type}_ndummy={cfg.model.n_dummy_radars}',
                               cfg.datasource.name, cfg.season, str(year))
@@ -23,7 +33,7 @@ def run(cfg: DictConfig):
             datasets.prepare_features(target_dir, osp.join(data_root, 'raw'), str(year), cfg.datasource.name,
                              random_seed=cfg.seed, edge_type=cfg.model.edge_type,
                              n_dummy_radars=cfg.model.n_dummy_radars, **cfg,
-                             process_dynamic=True, buffer_x=4, buffer_y=4)
+                             process_dynamic=True)
         else:
             print(f'year {year}: nothing to be done')
 
