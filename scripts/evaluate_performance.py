fc568f6153096ad14eaa0c0c19b963f681a83f35
FionaLippert <fiona@lipperta.de>
2021-09-30 13:04:34 +0200

    adjust evaluation

--- scripts/evaluate_performance.py
+++ scripts/evaluate_performance.py
@@ -19,24 +19,29 @@ def load_cv_results(result_dir, ext='', trials=1):
     return results
 
 
-def compute_rmse(model, experiment, results, groupby='trial', threshold=0, km2=True):
+def compute_mse(model, experiment, results, groupby='trial', threshold=0, km2=True):
     ext = '_km2' if km2 else ''
 
     results[f'squared_error{ext}'] = results[f'residual{ext}'].pow(2)
-    df = results.query(f'missing == 0 & gt{ext} >= {threshold} & night == 1')
-    rmse = df.groupby(groupby)[f'squared_error{ext}'].aggregate(np.mean).apply(np.sqrt)
-    rmse = rmse.reset_index(name='rmse')
-    rmse['model'] = model
-    rmse['experiment'] = experiment
+    df = results.query(f'missing == 0 & gt{ext} >= {threshold}') # & night == 1')
+    mse = df.groupby(groupby)[f'squared_error{ext}'].aggregate(np.mean) #.apply(np.sqrt)
+    mse = mse.reset_index(name='rmse')
+    mse['model'] = model
+    mse['experiment'] = experiment
 
-    return rmse
+    return mse
 
 if __name__ == "__main__":
 
     models = ['GAM', 'LocalMLP', 'LocalLSTM', 'FluxGraphLSTM']
-    models = { #'LocalLSTM': ['final_evaluation', 'final_evaluation_no_encoder'],
-              'LocalMLP': ['final_evaluation']} #, 'final_evaluation_new']}
-    models = {'GAM': ['final_evaluation']}
+
+    models = {  'FluxGraphLSTM': ['final_evaluation_0', 'final_evaluation_1', 'final_evaluation_2,'
+                                 'final_evaluation_0_6', 'final_evaluation_0_12', 'final_evaluation_0_48'],
+                'LocalLSTM': ['final_evaluation', 'final_evaluation_no_encoder'],
+                'LocalMLP': ['final_evaluation'],
+                'GAM': ['final_evaluation'],
+                'GBT': ['final_evaluation']
+             }
 
     trials = 5
     year = 2017
@@ -44,7 +49,11 @@ if __name__ == "__main__":
     ext = '_fixedT0'
     ext = ''
     base_dir = '/home/fiona/birdMigration/results'
+    base_dir = '/media/flipper/Seagate Basic/PhD/paper_1/results/radar'
+    #base_dir = '/media/flipper/Seagate Basic/PhD/paper_1/results/abm'
+    base_dir = '/home/flipper/birdMigration/results/radar'
     for m, dirs in models.items():
+        print(f'evaluate {m}')
         for d in dirs:
             result_dir = osp.join(base_dir, m, f'test_{year}', d)
             results = load_cv_results(result_dir, ext=ext, trials=trials)
@@ -52,9 +61,8 @@ if __name__ == "__main__":
             os.makedirs(output_dir, exist_ok=True)
 
             for thr in thresholds:
-                rmse_per_fold = compute_rmse(m, d, results, groupby='trial', threshold=thr, km2=True)
+                rmse_per_fold = compute_mse(m, d, results, groupby='trial', threshold=thr, km2=True)
                 rmse_per_fold.to_csv(osp.join(output_dir, f'rmse_thr{thr}{ext}.csv'))
 
-            thr = 0
-            rmse_per_hour = compute_rmse(m, d, results, groupby=['horizon', 'trial'], threshold=thr, km2=True)
-            rmse_per_hour.to_csv(osp.join(output_dir, f'rmse_per_hour_thr{thr}{ext}.csv'))
+                rmse_per_hour = compute_mse(m, d, results, groupby=['horizon', 'radar', 'trial'], threshold=thr, km2=True)
+                rmse_per_hour.to_csv(osp.join(output_dir, f'rmse_per_hour_thr{thr}{ext}.csv'))
