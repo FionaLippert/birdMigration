67935b0847885f04eb0be6ed1cfc79d25c74963c
FionaLippert <fiona@lipperta.de>
2021-10-18 17:11:09 +0200

    fix horizon in testing

--- scripts/run_NNs.py
+++ scripts/run_NNs.py
@@ -527,7 +527,7 @@ def testing(cfg: DictConfig, output_dir: str, log, ext=''):
             results['tidx'].append(_tidx)
             results['datetime'].append(time[_tidx])
             results['trial'].append([cfg.get('job_id', 0)] * y.shape[1])
-            results['horizon'].append(np.arange(-cfg.model.context-1, cfg.model.test_horizon+1))
+            results['horizon'].append(np.arange(-cfg.model.context, cfg.model.test_horizon+1))
             results['missing'].append(missing[ridx, :])
 
             if 'Flux' in cfg.model.name:
