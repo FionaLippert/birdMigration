f2a56fb244fb021b3d3230cea5d01990293cf4f4
FionaLippert <fiona@lipperta.de>
2023-10-04 15:18:00 +0200

    nexrad preprocessing

--- dataloading/run_nexrad_vpts2vpi.py
+++ dataloading/run_nexrad_vpts2vpi.py
@@ -1,5 +1,5 @@
 import subprocess
-import os
+import os, glob
 import os.path as osp
 import yaml
 import argparse
@@ -15,36 +15,56 @@ args = parser.parse_args()
 with open('nexrad_config.yml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-ts_str = config['ts'].strftime("%Y%m%dT%H%M")
-te_str = config['te'].strftime("%Y%m%dT%H%M")
-year = config['ts'].strftime("%Y")
+years = config['years']
+ts_str = config['ts']
+te_str = config['te']
 
-output_dir = osp.join(args.output_dir, year)
-os.makedirs(output_dir, exist_ok = True)
+for y in years:
 
-with open(os.path.join(output_dir, 'config.yml'), 'w+') as f:
-    yaml.dump(config, f)
-logfile = os.path.join(output_dir, 'log.txt')
+    print(f'process data for year {y}')
 
-start_time = datetime.now()
+    y = str(y)
 
-processes = set()
-max_processes = mp.cpu_count() - 1
+    config['ts'] = datetime.strptime(ts_str.replace('YEAR', y), "%Y-%m-%d %H:%M:%S")
+    config['te'] = datetime.strptime(te_str.replace('YEAR', y), "%Y-%m-%d %H:%M:%S")
 
-for r in config['radars']:
-    radar_file = osp.join(args.data_dir, year, f'{r}{year}.rds')
-    processes.add(subprocess.Popen(['Rscript', 'vpts2vpi.R', output_dir, radar_file],
-                            stdout=open(logfile, 'a+'),
-                            stderr=open(logfile, 'a+')))
-    if len(processes) >= max_processes:
-        os.wait()
-        processes.difference_update(
-            [p for p in processes if p.poll() is not None])
+    ts_y = config['ts'].strftime("%Y%m%dT%H%M")
+    te_y = config['te'].strftime("%Y%m%dT%H%M")
+    #year = config['ts'].strftime("%Y")
 
-#Check if all the child processes were closed
-for p in processes:
-    if p.poll() is None:
-        p.wait()
+    output_dir = osp.join(args.output_dir, y)
+    print(output_dir)
+    os.makedirs(output_dir, exist_ok = True)
+
+    with open(os.path.join(output_dir, 'config.yml'), 'w+') as f:
+        yaml.dump(config, f)
+    logfile = os.path.join(output_dir, 'log.txt')
+
+    start_time = datetime.now()
+
+    processes = set()
+    max_processes = mp.cpu_count() - 1
+
+    radar_list = config.get('radars', [])
+    for file in os.listdir(osp.join(args.data_dir, y)):
+        if file.endswith(f'{y}.rds'):
+            r = file.split(y)[0] # get radar name
+            if len(radar_list) == 0 or r in radar_list:
+                # start process for radar r
+                radar_file = osp.join(args.data_dir, y, file)
+                print(radar_file)
+                processes.add(subprocess.Popen(['Rscript', 'vpts2vpi.R', output_dir, radar_file],
+                                        stdout=open(logfile, 'a+'),
+                                        stderr=open(logfile, 'a+')))
+                if len(processes) >= max_processes:
+                    os.wait()
+                    processes.difference_update(
+                        [p for p in processes if p.poll() is not None])
+
+    #Check if all the child processes were closed
+    for p in processes:
+        if p.poll() is None:
+            p.wait()
 
 
 time_elapsed = datetime.now() - start_time
